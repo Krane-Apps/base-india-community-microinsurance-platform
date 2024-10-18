88978cb48572958df7aa395f8a5aaf5e3d531f68
Lovish Badlani <42366382+LovishB@users.noreply.github.com>
2024-10-18 12:14:57 +0530

    Update index.js

--- backend-app/index.js
+++ backend-app/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 CORS_CONFIG
 */
 const corsOptions = {
-  origin: 'https://cropsafe-base-sea-hackathon.vercel.app',
+  origin: 'https://cropsafe.vercel.app',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
